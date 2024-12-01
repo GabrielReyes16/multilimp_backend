806716e8ad1b5f58322cbe0d5fe926184c4f85a0
GabrielReyes16 <gabriel.reyes.b@tecsup.edu.pe>
2024-12-01 01:51:49 -0500

    test database

--- database/seeders/OpProductosSeeder.php
+++ database/seeders/OpProductosSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\MODELS\OpProducto;
+use App\Models\OpProducto;
 
 class OpProductosSeeder extends Seeder
 {
